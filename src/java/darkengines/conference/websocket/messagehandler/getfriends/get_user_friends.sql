d7a49e02ef0676b2624246528727845e201f20fd
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2013-03-28 22:30:36 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/Nexus@416 302f0312-3c5d-0d4b-b714-136c201f073e

--- src/java/darkengines/conference/websocket/messagehandler/getfriends/get_user_friends.sql
+++ src/java/darkengines/conference/websocket/messagehandler/getfriends/get_user_friends.sql
@@ -1,4 +1,4 @@
-SELECT u.`id`, u.`email`, u.`display_name` FROM `friendship` AS f 
-LEFT JOIN `user` AS u ON f.target = u.id
-INNER JOIN friendship AS fb ON fb.owner = u.id
+SELECT u.id, u.email, u.display_name FROM friendship AS f
+JOIN friendship AS fb ON f.owner = fb.target AND f.target = fb.owner
+JOIN `user` AS u ON u.id = f.target
 WHERE f.owner = ?;
